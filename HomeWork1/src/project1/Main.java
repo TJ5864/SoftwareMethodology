ccb808097dff10b538e237d00adc1dd7eef698f1
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2026-02-02 11:51:54 -0500

    Added Date Class

---
+++ HomeWork1/src/project1/Main.java
@@ -0,0 +1,8 @@
+package project1;
+
+public class Main {
+    //*  This is the project1.Main method where we will run all the other methods and classes
+    // @author Thomas Thomassen
+    // @param
+    // @return*/
+}
--- HomeWork1/src/project1/main.java
+++
@@ -1,8 +0,0 @@
-package project1;
-
-public class main {
-    //*  This is the project1.main method where we will run all the other methods and classes
-    // @author Thomas Thomassen
-    // @param
-    // @return*/
-}
