5f7682986d064af9d64c3c56921bec0eff4966aa
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2026-02-02 21:56:52 -0500

    modified Date class

--- HomeWork1/src/project1/Date.java
+++ HomeWork1/src/project1/Date.java
@@ -1,4 +1,59 @@
 package project1;
+/**
+ * @author tjt97
+ * creates the date variables
+ *
+ *            */
+
 
 public class Date {
+    private static final int MIN_YEAR = 1900;
+    private static final int MIN_MONTH = 1;
+    private static final int MAX_MONTH = 12;
+    private static final int FEBRUARY = 2;
+
+    private int year;
+    private int month;
+    private int day;
+
+/**
+ * create the date object with month day and year
+ * @param year tells us the year
+ * @param month tells us the month
+ * @param day tells us the day*/
+    public Date(int month, int day, int year) {
+        this.month = month;
+        this.day = day;
+        this.year=year;
+
+    }
+    /** Method tells us weather the date is valis
+     * @return true if the date is valid false if not*/
+    public boolean isValid(){
+        if(year < 0) return false;
+        if(month < 1 || month > 12) return false;
+        int [] daysInMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        if (isLeapYear()) {
+            daysInMonth[1] = 29;
+        }
+        if (day < 1 || day > daysInMonth[month - 1]) return false;
+
+        return true;
+
+    }
+    /** Tells us if the year is leap year and we need to adjust days
+     * @return true if leapyear false otherwise */
+    private boolean isLeapYear(){
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        return year % 4 == 0;
+    }
+
+    /** toString method convert the numeric date into a string ready to be output
+     * @return the month/day/year as a string */
+    @Override
+    public String toString(){
+        return month + "/" + day + "/" + year;
+    }
 }
--- HomeWork1/src/project1/Main.java
+++ HomeWork1/src/project1/Main.java
@@ -1,8 +1,20 @@
 package project1;
 
+import java.util.Scanner;
 public class Main {
     //*  This is the project1.Main method where we will run all the other methods and classes
     // @author Thomas Thomassen
     // @param
     // @return*/
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Date d1 = new Date(2,29,2024);
+        System.out.println(d1.isValid()); // true
+
+        Date d2 = new Date(2,29,2023);
+        System.out.println(d2.isValid()); // false
+
+
+    }
 }
