a58f829cf3e4930b462dbcf2dc18f33aec88a997
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2026-02-20 19:24:49 -0500

    Started List class

--- HomeWork2/src/util/List.java
+++ HomeWork2/src/util/List.java
@@ -1,4 +1,47 @@
 package util;
+import java.util.Iterator;
 
-public class List {
+public class List<E> implements Iterable<E> {
+    private static final int CAPACITY = 4;
+    private E[] objects;
+    private int size;
+    private final int NOT_FOUND = -1;
+
+    @SuppressWarnings("unchecked")
+    public List() {
+        objects = (E[]) new Object[CAPACITY];
+        size = 0;
+
+    }
+/** Find is the same as find in student list, looks for object in list E
+ * @param e object we are looking for
+ * @return position of e in the list or -1 if not in the list*/
+    private int find(E e){
+        for(int i = 0; i<size; i++){
+            if(objects[i].equals(e)){
+                return i;
+            }
+        }
+        return NOT_FOUND;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<E> {
+        private int current = 0;
+
+        @Override
+        public boolean hasNext() {
+            return current < size;
+        }
+
+        @Override
+        public E next() {
+            return objects[current++];
+        }
+    }
+
 }
