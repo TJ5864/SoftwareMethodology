ede65662f940b84f0ac517dcd22833c142b27a43
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2026-02-21 09:28:11 -0500

    Finished List.java methods

--- HomeWork2/src/util/List.java
+++ HomeWork2/src/util/List.java
@@ -7,7 +7,7 @@ public class List<E> implements Iterable<E> {
     private int size;
     private final int NOT_FOUND = -1;
 
-    @SuppressWarnings("unchecked")
+
     public List() {
         objects = (E[]) new Object[CAPACITY];
         size = 0;
@@ -70,7 +70,30 @@ public class List<E> implements Iterable<E> {
     public int size() {
         return size;
     }
+/** Get method returns the object at the given index
+ * @param index the index of the object we want
+ * @return the object located at the index*/
+    public E get(int index){
+        if(index < 0 || index >= size){
+            return null;
+        }
+        return objects[index];
+    }
 
+    /** Set methods sets and object e to a given index in list
+     * @param index the index we want to replace
+     * @param e the object we want to put at the given index*/
+    public void set(int index, E e){
+        if(index < 0 || index >= size){
+            return;
+        }
+        objects[index] = e;
+
+    }
+
+    public int indexOf(E e){
+        return find(e);
+    }
 
 
 
