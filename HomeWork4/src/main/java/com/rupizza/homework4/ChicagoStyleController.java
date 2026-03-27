eb007a901eb13669ed8b81599403da41a09d797e
MostafaSSh <mostafa.shalan99@gmail.com>
2026-03-27 10:13:08 -0400

    added chicago style GUI. further development necessary.

---
+++ HomeWork4/src/main/java/com/rupizza/homework4/ChicagoStyleController.java
@@ -0,0 +1,14 @@
+package main.java.com.rupizza.homework4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ChicagoStyleController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
