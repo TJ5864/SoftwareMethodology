143d1613742942ace848390980edb1674b989dee
TJ5864 <137420132+TJ5864@users.noreply.github.com>
2026-04-15 15:54:16 -0400

    Worked on App Home Screen

--- HomeWork5/app/src/main/java/com/example/myapplication/MainActivity.java
+++ HomeWork5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
 
+/** Home screen Activity. Entry point of the RU Pizza app. */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnOrderPizza = findViewById(R.id.btnOrderPizza);
+        Button btnCurrentOrder = findViewById(R.id.btnCurrentOrder);
+        Button btnAllOrders = findViewById(R.id.btnAllOrders);
+
+        btnOrderPizza.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PizzaMenuActivity.class);
+            startActivity(intent);
+        });
+
+        btnCurrentOrder.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CurrentOrderActivity.class);
+            startActivity(intent);
+        });
+
+        btnAllOrders.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AllOrdersActivity.class);
+            startActivity(intent);
         });
     }
 }
